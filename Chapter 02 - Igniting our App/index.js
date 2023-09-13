499d72817e0a450ab02bfce97df4a95aa1c52f0f
R3MO DAS <remodas7774@gmail.com>
2023-09-14 02:02:56 +0530

    parcel features

---
+++ Chapter 02 - Igniting our App/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const h1 = React.createElement("h1",{},"Chapter 02 - Igniting our App 🚀")
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(h1)
