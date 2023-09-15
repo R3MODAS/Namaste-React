bd20e2def6906776236a26149491959843a4a98a
R3MODAS <remodas7774@gmail.com>
2023-09-15 16:38:34 +0530

    react functional component basics

--- Chapter 03 - Laying the Foundation/index.js
+++ Chapter 03 - Laying the Foundation/index.js
@@ -2,8 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // const h1 = React.createElement("h1",{}, "Chapter 03 - Laying the Foundation 🚀");
+
+//! Welcome to JSX
+// const h1 = <h1>Chapter 03 - Laying the Foundation 🚀</h1>;
 // const root = ReactDOM.createRoot(document.querySelector("#root"));
 // root.render(h1);
 
-//! Welcome to JSX
-const jsxHeading = <h1>Chapter 03 - Laying the Foundation 🚀</h1>
+//! React Component -> Class Based and Functional Component
+
+//! Functional Component
+// const HeadingComponent = () => {
+//     return <h1>Namaste React Functional Component</h1>;
+// }
+
+const HeadingComponent = () => (<h1>Namaste React Functional Component</h1>)
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<HeadingComponent/>);
