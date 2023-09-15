e6b40a2d0fdbb885429c52cd487de6f74b3c80b2
R3MODAS <remodas7774@gmail.com>
2023-09-15 14:01:22 +0530

    chapter 3 - ongoing

---
+++ Chapter 03 - Laying the Foundation/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const h1 = React.createElement("h1",{}, "Chapter 03 - Laying the Foundation 🚀");
+// const root = ReactDOM.createRoot(document.querySelector("#root"));
+// root.render(h1);
+
+//! Welcome to JSX
+const jsxHeading = <h1>Chapter 03 - Laying the Foundation 🚀</h1>
