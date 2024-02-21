a110c32c3cbe8b8e59387a82cd3c502ace82956f
Sharadindu Das <remodas7774@gmail.com>
2024-02-22 00:05:04 +0530

    food delivery app

--- Youtube Clone/youtube-clone/src/components/SuggestionList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SuggestionList = () => {
-  return (
-    <div>SuggestionList</div>
-  )
-}
-
-export default SuggestionList
--- Youtube Clone/youtube-clone/src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=";
-
-export const YOUTUBE_SEARCH_SUGGESTION_API = "https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
-
-
