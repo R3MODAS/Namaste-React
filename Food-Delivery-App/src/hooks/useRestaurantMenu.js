82118f268ef04b7ec5d3eefca0a8e43ce2e2f04b
Sharadindu Das <remodas7774@gmail.com>
2024-03-31 11:57:46 +0530

    finalized food delivery app code updated

--- Food-Delivery-App/src/hooks/useRestaurantMenu.js
+++ Food-Delivery-App/src/hooks/useRestaurantMenu.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
-import { CORSPROXY } from "../utils/constants";
 
 const useRestaurantMenu = (resId) => {
     const [RestaurantMenuDetails, setRestaurantMenuDetails] = useState([])
@@ -9,15 +8,13 @@ const useRestaurantMenu = (resId) => {
     const lat = userLocation?.lat ? userLocation?.lat : 22.51800
     const lng = userLocation?.lng ? userLocation?.lng : 88.38320
 
-    const RES_MENU_API = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&restaurantId=${resId}`
-
     useEffect(() => {
         fetchRestaurantMenu();
     }, [])
 
     const fetchRestaurantMenu = async () => {
         try {
-            const response = await fetch(CORSPROXY + encodeURIComponent(RES_MENU_API))
+            const response = await fetch(import.meta.env.VITE_BASE_URL + `api/proxy/swiggy/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&restaurantId=${resId}`)
             if (!response.ok) {
                 const err = response.status;
                 throw new Error(err)
