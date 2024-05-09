d446d66f06b9328805b213cd19f541e731c7f336
Sharadindu Das <remodas7774@gmail.com>
2024-05-09 16:00:00 +0530

    revision of inception

--- Chapter 01 - Inception/script.js
+++ Chapter 01 - Inception/script.js
@@ -1,70 +1,73 @@
-//! ============== Hello world using JS =================
-// const heading = document.createElement("h1");
-// heading.innerHTML = "Hello world!";
+//! ==============  Vanilla JS =================
+// const heading = document.createElement("h1")
+// heading.innerText = "Hello World!"
 // document.querySelector("#root").appendChild(heading)
 
 //! ============== React SuperPowers ===================
-// const heading = React.createElement("h1", {id : "heading"}, "Hello world !");
-// const root = ReactDOM.createRoot(document.querySelector("#root"));
-// root.render(heading);
+// const heading = React.createElement("h1", {id: "heading"}, "Hello World!")
+// const root = ReactDOM.createRoot(document.querySelector("#root"))
+// root.render(heading)
 
-//! Nested HTML
-/*
-        <div id="parent">
-            <div id="child">
-                <h1>This is heading 1</h1>
-            </div>
+// console.log(heading);
+// console.log(typeof heading);
+
+//! ============== Rendering Nested React Elements ==============
+{/* 
+    <div id="parent">
+        <div id="child">
+            <h1>This is a heading</h1>
         </div>
-*/
+    </div> 
+*/}
 
-/*
-        <div id="parent">
-            <div id="child">
-                <h1>This is heading 1</h1>
-                <h2>This is heading 2</h2>
-            </div>
-        </div>
-*/
-
-/*
-        <div id="parent">
-            <div id="child1">
-                <h1>This is heading 1</h1>
-                <h2>This is heading 2</h2>
-            </div>
-            <div id="child2">
-                <h1>This is heading 1</h1>
-                <h2>This is heading 2</h2>
-            </div>
-        </div>
-*/
-
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement("div",{ id: "child" }, [
-//     React.createElement("h1", {id : "heading"}, "This is heading 1"),
-//     React.createElement("h2", {id : "heading"}, "This is heading 2")
-// ])
-// );
-
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//     [
-//         React.createElement("div",{ id: "child1" }, [
-//             React.createElement("h1", {id : "heading"}, "This is heading 1"),
-//             React.createElement("h2", {id : "heading"}, "This is heading 2")
-//         ]),
-//         React.createElement("div",{ id: "child2" }, [
-//             React.createElement("h1", {id : "heading"}, "This is heading 1"),
-//             React.createElement("h2", {id : "heading"}, "This is heading 2")
-//         ])
-//     ]
-// );
-
-// const root = ReactDOM.createRoot(document.querySelector("#root"));
+// const parent = React.createElement("div", {id: "parent"}, 
+//     React.createElement("div", {id: "child"}, 
+//         React.createElement("h1", {id: "heading"}, "This is a Nested React Element")
+//     )
+// )
+// const root = ReactDOM.createRoot(document.querySelector("#root"))
 // root.render(parent)
-// console.log(parent)
 
+{/* 
+    <div id="parent">
+        <div id="child">
+            <h1>This is a heading 1</h1>
+            <h2>This is heading 2</h2>
+        </div>
+    </div> 
+*/}
 
+// const parent = React.createElement("div", { id: "parent" },
+//     React.createElement("div", { id: "child" }, [
+//         React.createElement("h1", { className: "common-heading" }, "This is heading 1"),
+//         React.createElement("h2", { className: "common-heading" }, "This is heading 2")
+//     ])
+// )
+// const root = ReactDOM.createRoot(document.querySelector("#root"))
+// root.render(parent)
+
+{/* 
+    <div id="parent">
+        <div id="child1">
+            <h1>This is heading 1</h1>
+            <h2>This is heading 2</h2>
+        </div>
+        <div id="child2">
+            <h1>This is heading 1</h1>
+            <h2>This is heading 2</h2>
+        </div>
+    </div> 
+*/}
+
+// const parent = React.createElement("div", {id: "parent"}, [
+//     React.createElement("div", {id: "child1"}, [
+//         React.createElement("h1", {}, "This is heading 1 of child 1"),
+//         React.createElement("h2", {}, "This is heading 2 of child 1")
+//     ]),
+//     React.createElement("div", {id: "child2"}, [
+//         React.createElement("h1", {}, "This is heading 1 of child 2"),
+//         React.createElement("h2", {}, "This is heading 2 of child 2")
+//     ])
+// ])
+// const root = ReactDOM.createRoot(document.querySelector("#root"))
+// root.render(parent)
